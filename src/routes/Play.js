7b6e02576f140141b0136e28f9e950c266cdea95
xamma <Xam93@live.de>
2022-12-09 15:11:42 +0100

    Button with styled component.

--- src/routes/Play.js
+++ src/routes/Play.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import styled from 'styled-components';
 
 const Play = () => {
+
+  const Button = styled.button`
+  background-color: black;
+  color: white;
+  font-size: 20px;
+  padding: 10px 60px;
+  border-radius: 5px;
+  margin: 10px 0px;
+  cursor: pointer;
+`;
+// Usage
   return (
-    <div>Play</div>
+    <div>
+      <Button onClick={() => console.log("test")}>
+        Button
+      </Button>
+    </div>
   )
 }
 
